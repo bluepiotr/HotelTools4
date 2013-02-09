8b2af945371a97100a8e1318334d9ea1072283ac
bluepiotr <bluepit@yahoo.com>
2013-02-09 22:52:26 +0000

    @RequiredArgsConstructor creation and push upstream

--- Hotel/src/hotel/Hotel.java
+++ Hotel/src/hotel/Hotel.java
@@ -2,17 +2,18 @@ package hotel;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.AccessLevel;
 
+@RequiredArgsConstructor
 public class Hotel {
 	
-	@Getter @Setter (AccessLevel.PACKAGE) private String name;	
+	@Getter (AccessLevel.PACKAGE) private final String name;	
 	@Getter @Setter private String address;	
 	@Getter @Setter private String telephone;
-	@Getter @Setter private int numberOfRooms;
+	@Getter @Setter @NonNull private int numberOfRooms;
 	@Getter @Setter private int starRating;
-	@Getter @Setter private String manager;	
-	
-	
+	@Getter @Setter @NonNull private String manager;		
 }
 
--- Hotel/src/hotel/HotelTester.java
+++ Hotel/src/hotel/HotelTester.java
@@ -3,9 +3,9 @@ package hotel;
 public class HotelTester {
 	
 	public static void main (String[] args){
-		Hotel h = new Hotel ();
-		h.setName("Cosy Quarters");
-		System.out.println(h.getName());
+		Hotel h = new Hotel ("Cosy Quarters", 120, "Jon Bon");
+		System.out.println("hotel " + "'" + h.getName() 
+				+ "'" + "has been created");
 	}
 
 }
